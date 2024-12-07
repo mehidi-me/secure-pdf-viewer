99823c4b1b9f9b901291d0acae8281b98955e2e5
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-12-08 02:41:21 +0600

    init3

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
+import multer from "multer";
 import path from "path";
 import fs from "fs/promises";
 
-
+const upload = multer({ dest: "uploads/" });
 
 function runMiddleware(req, fn) {
   return new Promise((resolve, reject) => {
